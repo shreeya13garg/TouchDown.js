5d7b2dcff4c63b51c8eb2062feba9d902c22e3c5
HackGod2000 <45161513+HackGod2000@users.noreply.github.com>
2020-07-05 21:57:53 +0530

    minor-fix1

--- three-tuts/src/Components/Model.jsx
+++ three-tuts/src/Components/Model.jsx
@@ -59,13 +59,13 @@ function Mars(props) {
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   const imgmars = useMemo(() => new TextureLoader().load(marsURL), [marsURL]);
-  const bumpmap = useMemo(() => new TextureLoader().load(bumperURL), [
+  const bumpmap1 = useMemo(() => new TextureLoader().load(bumperURL), [
     bumperURL,
   ]);
   console.log(marsURL);
 
   // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => (mesh.current.rotation.y += 0.01));
+  useFrame(() => (mesh.current.rotation.y += 0.006));
 
   return (
     <mesh
@@ -81,7 +81,7 @@ function Mars(props) {
         attach="material"
         //color={hovered ? "hotpink" : "orange"}
         map={imgmars}
-        bumpmap={bumperURL}
+        bumpmap={bumpmap1}
         bumpscale={0.1}
       />
     </mesh>
