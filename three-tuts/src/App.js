350947e80c3bdc3c501f19ad197166e143ae70b1
HackGod2000 <45161513+HackGod2000@users.noreply.github.com>
2020-07-10 12:50:41 +0530

    removed all duplicates

--- three-tuts/src/App.js
+++ three-tuts/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 //import "./App.css";
 import Model from "./Components/Model";
-
+import PathfindingVisualizer from "./Components/PathfindingVisualizer";
 function App() {
   return (
-    <div className="App">
-      <Model></Model>
-    </div>
+    <Router>
+      <Switch>
+        <div className='App'>
+          <Route exact path='/'>
+            <Model />
+          </Route>
+          <Route path='/algo'>
+            <PathfindingVisualizer />
+          </Route>
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
--- three-tuts/src/Components/Model.jsx
+++ three-tuts/src/Components/Model.jsx
@@ -7,7 +7,6 @@ import bumperURL from "./images/marsbump1k.jpg";
 import marsURL from "./images/marsmap1k.jpg";
 import "./Model.css";
 import StartPage from "./Startpage";
-
 console.log(bumperURL);
 const SpaceShip = () => {
   const [model, setModel] = useState();
@@ -24,41 +23,67 @@ class Model extends Component {
     super(props);
     this.state = {
       data: [],
+      scale: [1, 1, 1],
     };
+    this.zoom = this.zoom.bind(this);
+    this.enlarge = this.enlarge.bind(this);
   }
   componentDidMount() {
     const data = new Array(1000).fill(0).map((d, id) => ({ id }));
     this.setState({ data });
   }
-
+  enlarge(i, j, k) {
+    const scaled = parseFloat(`${i}.${j}${k}`, 10);
+    this.setState({ scale: [scaled, scaled, scaled] });
+  }
+  async zoom() {
+    for (let i = 1; i <= 2; i++) {
+      for (let j = 0; j <= 9; j++) {
+        for (let k = 0; k <= 9; k++) {
+          await this.enlarge(i, j, k);
+          await sleep(5);
+        }
+      }
+    }
+  }
   render() {
     const { data } = this.state;
-    //
+    //<SpaceShip />
     //
     //console.log(data);
     return (
       //  Sonali startpage.jsx controls the design element of the page.
-      //<SpaceShip />
 
       <>
         <StartPage />
-        <Canvas camera={{ position: [0, 0, 20] }}>
-          <ambientLight intensity={1.0} />
-          <pointLight intensity={0.9} position={[-10, -25, -10]} />
-          <spotLight
-            castShadow
-            intensity={0.25}
-            angle={Math.PI / 8}
-            position={[25, 25, 15]}
-            shadow-mapSize-width={2048}
-            shadow-mapSize-height={2048}
-          />
-          <Controls />
-          <Mars position={[1.2, 0, 0]} />
-        </Canvas>
+        <div
+          onClick={async () => {
+            await this.zoom();
+            window.location = "http://localhost:3000/algo";
+          }}
+          className="canvas-div"
+        >
+          <Canvas camera={{ position: [0, 0, 20] }}>
+            <ambientLight intensity={1.0} />
+            <pointLight intensity={0.9} position={[-10, -25, -10]} />
+            <spotLight
+              castShadow
+              intensity={0.25}
+              angle={Math.PI / 8}
+              position={[25, 25, 15]}
+              shadow-mapSize-width={2048}
+              shadow-mapSize-height={2048}
+            />
+            <Controls />
+            <Mars position={[1.2, 0, 0]} scale={this.state.scale} />
+          </Canvas>
+        </div>
       </>
     );
   }
+}
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function Mars(props) {
@@ -77,19 +102,18 @@ function Mars(props) {
     bumperURL,
   ]);
   console.log(marsURL);
-
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => (mesh.current.rotation.y += 0.006));
-
   return (
     <mesh
       {...props}
       ref={mesh}
-      scale={active ? [2.0, 2.0, 2.0] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
+      scale={props.scale}
+      onClick={(e) => {
+        setActive(!active);
+      }}
       onPointerOver={(e) => setHover(true)}
       onPointerOut={(e) => setHover(false)}
-      className="Mars"
     >
       <sphereGeometry attach="geometry" args={[6, 26, 26]} />
       <meshStandardMaterial
